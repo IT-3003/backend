960a0c5e08151d22e9ed237adc2809cb3b4867da
JaninduHasaranga <dilanjanidu@gmail.com>
2026-07-16 20:00:34 +0530

    Make some changes in my controll file

--- src/main/java/com/threefour/backend/Janindu/JaninduController.java
+++ src/main/java/com/threefour/backend/Janindu/JaninduController.java
@@ -13,11 +13,5 @@ public class JaninduController {
 
     }
 
-    @GetMapping("/getsum")
-    public String ht(){
-        int num1 = 10 ;
-        int num2 = 20 ;
-        return JaninduService.addnumbers(num1 , num2);
-    }
 
 }
