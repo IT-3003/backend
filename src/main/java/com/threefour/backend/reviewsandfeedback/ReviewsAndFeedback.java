d9a06cd2d57636e71eaa80fbcc4d97006e2fde8b
Prabhavi Jayawardana <prabhavijayawardana@gmail.com>
2026-07-16 23:21:37 +0530

    Added variables

--- src/main/java/com/threefour/backend/reviewsandfeedback/ReviewsAndFeedback.java
+++ src/main/java/com/threefour/backend/reviewsandfeedback/ReviewsAndFeedback.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jdk.jfr.Name;
 
 @Entity
 public class ReviewsAndFeedback {
@@ -11,6 +12,8 @@ public class ReviewsAndFeedback {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    public String name;
+    public int age;
 
     // Your other fields, getters, and setters
 }
