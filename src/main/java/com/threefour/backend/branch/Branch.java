cfa539195e412c38692dc470291882458ea7cba1
Himandi Ranawaka <himandiranawaka@gmail.com>
2026-07-18 22:42:27 +0530

    branch__

--- src/main/java/com/threefour/backend/branch/Branch.java
+++ src/main/java/com/threefour/backend/branch/Branch.java
@@ -1,12 +1,44 @@
 package com.threefour.backend.branch;
+
 import java.util.Date;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
 @Entity
-
 public class Branch {
+
+    @Id
+    private int branchId;
+
+    private String branchName;
+    private String address;
+    private String phoneNumber;
+    private int managerId;
+    private String openingHours;
+    private Date createdDate;
+    private Date updatedDate;
+
+    // Default Constructor
+    public Branch() {
+
+    }
+
+    // Parameterized Constructor
+    public Branch(int branchId, String branchName, String address, String phoneNumber,
+                  int managerId, String openingHours, Date createdDate, Date updatedDate) {
+        this.branchId = branchId;
+        this.branchName = branchName;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.managerId = managerId;
+        this.openingHours = openingHours;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
+    }
+
+    // Getters and Setters
+
     public int getBranchId() {
         return branchId;
     }
@@ -14,9 +46,6 @@ public class Branch {
     public void setBranchId(int branchId) {
         this.branchId = branchId;
     }
-
-    @Id
-    private int branchId;
 
     public String getBranchName() {
         return branchName;
@@ -26,8 +55,6 @@ public class Branch {
         this.branchName = branchName;
     }
 
-    private String branchName;
-
     public String getAddress() {
         return address;
     }
@@ -36,70 +63,43 @@ public class Branch {
         this.address = address;
     }
 
-    private String address;
-    private String phoneNumber;
-    private int managerId;
-
-    public String getOpeningHours() {
-        return openingHours;
-    }
-
     public String getPhoneNumber() {
         return phoneNumber;
-    }
-
-    public int getManagerId() {
-        return managerId;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public Date getUpdatedDate() {
-        return updatedDate;
-    }
-
-
-    private String openingHours;
-    private Date createdDate;
-    private Date updatedDate;
-
-
-    public void setUpdatedDate(Date updatedDate) {
-        this.updatedDate = updatedDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public void setOpeningHours(String openingHours) {
-        this.openingHours = openingHours;
-    }
-
-    public void setManagerId(int managerId) {
-        this.managerId = managerId;
     }
 
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
-    public Branch(int branchId, String branchName, String address, String phoneNumber, int managerId, String openingHours, Date createdDate, Date updatedDate) {
-        this.branchId = branchId;
-        this.branchName = branchName;
-        this.address = address;
-        this.phoneNumber = phoneNumber;
-        this.managerId = managerId;
-        this.openingHours = openingHours;
-        this.createdDate = createdDate;
-        this.updatedDate = updatedDate;
+    public int getManagerId() {
+        return managerId;
     }
 
+    public void setManagerId(int managerId) {
+        this.managerId = managerId;
+    }
 
-    public Branch() {
+    public String getOpeningHours() {
+        return openingHours;
+    }
 
+    public void setOpeningHours(String openingHours) {
+        this.openingHours = openingHours;
+    }
 
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(Date updatedDate) {
+        this.updatedDate = updatedDate;
     }
 }
