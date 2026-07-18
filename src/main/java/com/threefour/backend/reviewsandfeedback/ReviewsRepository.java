f61ac6fe62baa72e7b6ea84a0c6433c748b6f379
Prabhavi Jayawardana <prabhavijayawardana@gmail.com>
2026-07-18 23:04:17 +0530

    Added getters and setters

--- src/main/java/com/threefour/backend/reviewsandfeedback/ReviewsRepository.java
+++ src/main/java/com/threefour/backend/reviewsandfeedback/ReviewsRepository.java
@@ -1,11 +1,10 @@
 package com.threefour.backend.reviewsandfeedback;
 
-import com.threefour.backend.reviewsandfeedback.ReviewsAndFeedback;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 // Pass your Entity type (Product) and its ID type (Long) into the generic
-public interface ReviewsAndFeedbackRepository extends JpaRepository<ReviewsAndFeedback, Long> {
+public interface ReviewsRepository extends JpaRepository<Reviews, Long> {
     // Spring Boot automatically implements basic database methods here!
 }
