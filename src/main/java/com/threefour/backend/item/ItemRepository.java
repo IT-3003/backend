4e1658252f3e73c005b195c67a45339d0ff1dd20
Dinuvi02 <wdinuvi@gmail.com>
2026-07-19 09:35:50 +0530

    update table

--- src/main/java/com/threefour/backend/item/ItemRepository.java
+++ src/main/java/com/threefour/backend/item/ItemRepository.java
@@ -1,9 +1,9 @@
-package com.threefour.backend.dinuvi;
+package com.threefour.backend.item;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DinuviRepository extends JpaRepository<Dinuvi, Long> {
+public interface ItemRepository extends JpaRepository<Item, Long> {
 
 }
