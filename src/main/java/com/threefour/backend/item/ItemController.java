7c4479825a3ad15984485d3ac28b43674aa7ad0e
Dinuvi02 <wdinuvi@gmail.com>
2026-07-23 22:58:57 +0530

    creating GET and DELETE

--- src/main/java/com/threefour/backend/item/ItemController.java
+++ src/main/java/com/threefour/backend/item/ItemController.java
@@ -5,13 +5,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/dinuvi")
+@RequestMapping("/api/item")
 public class ItemController {
 
-    private final ItemService dinuviService;
+    private final ItemService itemService;
 
     public ItemController(ItemService dinuviService) {
-        this.dinuviService = dinuviService;
+        this.itemService = dinuviService;
     }
 
     @GetMapping
@@ -20,8 +20,26 @@ public class ItemController {
     }
 
     @PostMapping("/create")
-    public ResponseEntity<Item> create(@RequestBody Item dinuvi) {
-        Item savedDinuvi = dinuviService.saveDinuvi(dinuvi);
+    public ResponseEntity<Item> create(@RequestBody Item item) {
+        Item savedDinuvi = itemService.saveItem(item);
         return new ResponseEntity<>(savedDinuvi, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public Item getItemById(@PathVariable Long id) {
+        return itemService.getItemById(id);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Item> updateItem(@PathVariable Long id,
+                                           @RequestBody Item item) {
+
+        Item updatedItem = itemService.updateItem(id, item);
+        return ResponseEntity.ok(updatedItem);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteItem(@PathVariable Long id) {
+        itemService.deleteItem(id);
+        return ResponseEntity.ok("Item deleted successfully.");
     }
 }
--- src/main/java/com/threefour/backend/item/ItemService.java
+++ src/main/java/com/threefour/backend/item/ItemService.java
@@ -5,17 +5,37 @@ import org.springframework.stereotype.Service;
 @Service
 public class ItemService {
 
-    private final ItemRepository dinuviRepository;
+    private final ItemRepository itemRepository;
 
-    public ItemService(ItemRepository dinuviRepository) {
-        this.dinuviRepository = dinuviRepository;
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
     }
 
-    public String addNumbers(int a, int b) {
-        return "The sum is " + (a + b);
+    public Item saveItem(Item item) {
+        return itemRepository.save(item);
+    }
+    public Item getItemById(Long id) {
+        // Fetches all columns for the specific primary key
+        return itemRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found with id: " + id));
     }
 
-    public Item saveDinuvi(Item dinuvi) {
-        return dinuviRepository.save(dinuvi);
+    public void deleteItem(Long id) {
+        Item item = itemRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Item not found with id: " + id));
+
+        itemRepository.delete(item);
+    }
+
+    public Item updateItem(Long id, Item updatedItem) {
+
+        Item item = itemRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Item not found with id: " + id));
+
+        item.setitemName(updatedItem.getitemName());
+        item.setCategory(updatedItem.getCategory());
+        item.setBaseprice(updatedItem.getBaseprice());
+
+        return itemRepository.save(item);
     }
 }
