3a488b4fe50925c29e003f4a99864976916aaaec
Himandi Ranawaka <himandiranawaka@gmail.com>
2026-07-23 22:54:54 +0530

    creating GET and DELETE endpoints for branch

--- src/main/java/com/threefour/backend/branch/BranchController.java
+++ src/main/java/com/threefour/backend/branch/BranchController.java
@@ -13,15 +13,26 @@ public class BranchController {
 
     private final BranchService branchService;
 
+
     @GetMapping("/hasi")
     public String hello() {
         return "Hello Himandiiiiiii";
     }
     @PostMapping("/create")
     public ResponseEntity<Branch> create(@RequestBody Branch branch) {
-        Branch savedBranch = branchService.saveHimandi(branch);
+        Branch savedBranch = branchService.savebranch(branch);
         return new ResponseEntity<>(savedBranch, HttpStatus.CREATED);
+    }
+    @GetMapping("/{branchid}")
+    public Branch getBranchById(@PathVariable int branchid) {
+        return branchService.getBranchById(branchid);
     }
 
 
-}
+
+        @DeleteMapping("/{branchid}")
+        public String deleteBranch(@PathVariable int branchid) {
+            branchService.deleteBranch(branchid);
+            return "Branch deleted successfully.";
+        }
+    }
