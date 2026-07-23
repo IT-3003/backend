bc5fceeaebd790ca0a695b4408aa7d3ceedcf89c
Thinula Harischandra <134825200+thinulaH@users.noreply.github.com>
2026-07-23 14:07:05 +0530

    Fix duplicate RequestMapping path in PromotionController

--- src/main/java/com/threefour/backend/promotion/PromotionController.java
+++ src/main/java/com/threefour/backend/promotion/PromotionController.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/promotion")
 public class PromotionController {
     private final PromotionService anshuService;
 
