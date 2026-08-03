7024aba46aa3ce115944dbeed500ed07b0aa6ebf
JaninduHasaranga <dilanjanidu@gmail.com>
2026-08-03 10:51:42 +0530

    Fix errors in DELETE method

--- src/main/java/com/threefour/backend/payment/PaymentRepository.java
+++ src/main/java/com/threefour/backend/payment/PaymentRepository.java
@@ -5,11 +5,20 @@ package com.threefour.backend.payment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 // Pass your Entity type (Product) and its ID type (Long) into the generic
 public interface PaymentRepository extends JpaRepository<Payment, Integer> {
     // Spring Boot automatically implements basic database methods here!
     java.util.Optional<Payment> findByTransaction(String transaction);
+
+    @Transactional
+    void deleteByUser_Id(int userId);
+
+    @Transactional
+    void deleteByOrder_OrderId(int orderId);
+
+    @Transactional
+    void deleteByOrder_Branch_BranchId(int branchId);
 }
